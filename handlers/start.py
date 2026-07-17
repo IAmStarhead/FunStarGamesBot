5c07ecee7e22f906d4df80d51cfc9619983acd0e
IAmStarhead <robertst4r@yandex.ru>
2026-07-17 21:48:39 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
-from handlers import blackjack
+from handlers import blackjack, durak
 
 WELCOME_TEXT = (
     "🚀 Приветствую, {name}!\n\n"
